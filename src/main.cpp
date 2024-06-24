664460a202aab3eb8aa5c150d277a1132756e0f2
cyril-muzikaloid <cyril.makloufi@gmail.com>
2024-06-24 17:51:14 +0200

    Control speed of rotation

--- src/main.cpp
+++ src/main.cpp
@@ -8,23 +8,31 @@ Servo myServo;
 #define D_PIN 25
 #define SERVO_PIN 12
 
+uint8_t movingSpeed = 255;
+
 void stopMove()
 {
-  digitalWrite(G_PIN, LOW);
-  digitalWrite(D_PIN, LOW);
-  delay(1500);
+  analogWrite(G_PIN, 0);
+  analogWrite(D_PIN, 0);
+  delay(250);
 }
 
 void moveLeft()
 {
   stopMove();
-  digitalWrite(G_PIN, HIGH);
+  //digitalWrite(G_PIN, HIGH);
+  
+
+  analogWrite(G_PIN, movingSpeed);
 }
 
 void moveRight() 
 {
   stopMove();
-  digitalWrite(D_PIN, HIGH);
+  //digitalWrite(D_PIN, HIGH);
+ 
+
+  analogWrite(D_PIN, movingSpeed);
 }
 
 void shoot()
@@ -35,16 +43,19 @@ void shoot()
 }
 
 void setup() {
-  Serial.begin(9600);
+  //Serial.begin(9600);
+  analogWriteResolution(8);
+  analogWriteFrequency(100);
   myServo.attach(SERVO_PIN);
   myServo.write(40);
   //INIT_DEFAULT_SETTINGATOR();
   STR.SetCommunicator(ESPNowCTR::CreateInstanceDiscoverableWithSSID("Turret"));
   pinMode(G_PIN, OUTPUT);
   pinMode(D_PIN, OUTPUT);
-  digitalWrite(G_PIN, LOW);
-  digitalWrite(D_PIN, LOW);
+  analogWrite(G_PIN, 0);
+  analogWrite(D_PIN, 0);
 
+  STR.AddSetting(Setting::Type::Slider, &movingSpeed, sizeof(uint8_t), "SPEED");
   STR.AddSetting(Setting::Type::Trigger, nullptr, 0, "GAUCHE", &moveLeft);
   STR.AddSetting(Setting::Type::Trigger, nullptr, 0, "STOP", &stopMove);
   STR.AddSetting(Setting::Type::Trigger, nullptr, 0, "DROITE", &moveRight);
