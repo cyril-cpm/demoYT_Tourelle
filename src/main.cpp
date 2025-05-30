c3a98dabecc876216fa5aa4aa4de1da15fd6a8a7
cyril-muzikaloid <cyril.makloufi@gmail.com>
2025-05-30 02:21:00 +0200

    Upgrade Settingator to 0.3.10

--- src/main.cpp
+++ src/main.cpp
@@ -6,7 +6,7 @@
 #include <HardwareSerialCommunicator.h>
 #include <MiscDef.h>
 //#include <STR.h>
-#include "CustomType.h"
+#include "CustomType.hpp"
 
 #include <WebSocketCommunicator.h>
 
@@ -18,12 +18,12 @@ Servo motorServo(GPIO_NUM_27);
 
 bool displayMotorPos = false;
 
-
+#define CALLBACK_END_NOTIF 0x10
 
 Settingator STR(nullptr);
 
-STR_Float motorPos = 18.0f;
-STR_UInt32 fadingTimeMS = 0;
+STR_Float motorPos(0.0f, "POSITION");
+STR_Float motorSpeed(0.0f, "SPEED");
 
 void shoot()
 {
@@ -32,96 +32,30 @@ void shoot()
   triggerServo.write(40);
 }
 
-/*char* posLabel = (char*)malloc(6);
-uint8_t refPosLabel = 0;
-
-void updatePosLabel()
-{
-    if (displayMotorPos)
-    {
-        posLabel = itoa(motorPos, posLabel, 10);
-        STR.UpdateSetting(refPosLabel, (byte*)posLabel, sizeof(posLabel));
-    }
-}*/
-
-void updateServoMotor()
-{
-    motorServo.write(motorPos);
-}
-
-void balai()
-{
-    for (auto i = 0.0f; i <= 180.0f; i+=0.1f)
-    {
-        motorServo.write(i);
-        delay(20);
-    }
-}
-
 void setup() {
-  Serial.begin(115200);
-    fadingTimeMS.SetCallback([](){ motorServo.setFadingTimeMS(fadingTimeMS); });
+    Serial.begin(115200);
 
-    motorPos = 0.0f;
+    motorPos.SetCallback([](){
+        motorServo.write(motorPos);
+    });
+    motorSpeed.SetCallback([](){
+        motorServo.setSpeed(motorSpeed);
+    });
+    motorServo.setFadingCallback([](){ STR.SendNotif(CALLBACK_END_NOTIF); });
 
-  /*for (int i = 0; i < 3; i++)
-    posLabel[i] = '0';
+    motorServo.write(motorPos);
+    triggerServo.write(40);
 
-  posLabel[3] = '\0';*/
 
-  //STR_Float testFl = 18.0f;
+    //STR.SetCommunicator(HardwareSerialCTR::CreateInstance(115200));
+    STR.AddSetting(Setting::Type::Trigger, nullptr, 0, "SHOOT", &shoot);
 
-  triggerServo.write(40);
-
-  motorServo.write(0);
-
-  //delay(1000);
-
-  //balai();
-
-  STR.SetCommunicator(ESPNowCTR::CreateInstanceDiscoverableWithSSID("Turret"));
-
-  //INIT_DEFAULT_SETTINGATOR();
-  //STR.SetCommunicator(HardwareSerialCTR::CreateInstance(115200));
-  STR.AddSetting(Setting::Type::Trigger, nullptr, 0, "SHOOT", &shoot);
-
-  /*STR.AddSetting(Setting::Type::Slider, &motorPos, sizeof(motorPos), "POSITION", [](){
-    updatePosLabel();
-  });
-  // TEST MOTOR
-  STR.AddSetting(Setting::Type::Switch, &displayMotorPos, sizeof(displayMotorPos), "DISPLAY MOTOR POS");
-
-  STR.AddSetting(Setting::Type::Trigger, nullptr, 0, "+10", [](){
-    motorPos += 10;
-    if (motorPos > 180)
-        motorPos = 180;
-    updatePosLabel();
-});
-
-  STR.AddSetting(Setting::Type::Trigger, nullptr, 0, "++", [](){ 
-    if (motorPos < 180) 
-        motorPos++;
-    updatePosLabel();
-  });
-
-  refPosLabel = STR.AddSetting(Setting::Type::Label, posLabel, sizeof(posLabel));
-
-  STR.AddSetting(Setting::Type::Trigger, nullptr, 0, "--", [](){ 
-    if (motorPos > 0)
-        motorPos--;
-    updatePosLabel();
-  });
-
-  STR.AddSetting(Setting::Type::Trigger, nullptr, 0, "-10", [](){
-    motorPos -= 10;
-    if (motorPos > 180)
-        motorPos = 0;
-    updatePosLabel();
-  });*/
-  //motorPos.SetAutoUpdate(true);
+    ESPNowCore *espNowCore = nullptr;
+    espNowCore = ESPNowCore::CreateInstance();
+    if (espNowCore)
+        espNowCore->BroadcastPing();
 }
 
 void loop () {
-  STR.Update();
-  updateServoMotor();
+    STR.Update();
 }
